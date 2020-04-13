8397f7f94888e71e2876660bef8c5e64035ff306
Kauê Cavalcante <61801350+kauecdev@users.noreply.github.com>
2020-04-13 11:14:18 -0300

    Add files via upload

---
+++ Projeto_07/js/scripts.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    $('nav.desktop a').hover(function(){
+        $('nav li').removeClass('selected');
+        $(this).parent().addClass('selected');
+    });
+
+    $('nav.mobile h3').click(function(){
+        $('ul.nav-desktop').slideToggle();
+    });
+}
+    
